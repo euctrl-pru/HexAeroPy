88332b3aab54b8fc1150cc2801324d653bf66cd5
Quinten Goens <qgoens@gmail.com>
2024-02-12 17:47:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='HexAeroPy',
-    version='0.0.3',
+    version='1.0.1',
     packages=find_packages(),
     include_package_data=True,  
     package_data={
